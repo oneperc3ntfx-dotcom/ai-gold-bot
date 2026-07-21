04677ba92f12b674dd4291da806c5861eede0c4e
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-22 04:14:39 +0700

    Create buttons.py

---
+++ keyboards/buttons.py
@@ -0,0 +1,31 @@
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+
+
+
+
+def renew_button():
+
+
+    return InlineKeyboardMarkup(
+
+        inline_keyboard=[
+
+            [
+
+                InlineKeyboardButton(
+
+                    text="🔄 PERPANJANG MEMBERSHIP",
+
+                    url="https://t.me/Intradayxauusd_bot"
+
+                )
+
+            ]
+
+        ]
+
+    )
