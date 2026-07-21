f270fc4ef9335f7a61530157679b53f0ccefefd9
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-22 04:12:11 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from dotenv import load_dotenv
+
+
+from config.settings import BOT_TOKEN
+
+
+from handlers.start import router as start_router
+from handlers.menu import router as menu_router
+from handlers.membership import router as membership_router
+from handlers.signal import router as signal_router
+from handlers.admin import router as admin_router
+
+
+
+load_dotenv()
+
+
+
+bot = Bot(
+    token=BOT_TOKEN
+)
+
+
+
+dp = Dispatcher()
+
+
+
+# ==========================
+# REGISTER HANDLER
+# ==========================
+
+
+dp.include_router(
+    start_router
+)
+
+
+dp.include_router(
+    menu_router
+)
+
+
+dp.include_router(
+    membership_router
+)
+
+
+dp.include_router(
+    signal_router
+)
+
+
+dp.include_router(
+    admin_router
+)
+
+
+
+
+# ==========================
+# START BOT
+# ==========================
+
+
+async def main():
+
+
+    print(
+        "🤖 XAU AI SIGNAL BOT RUNNING..."
+    )
+
+
+    await dp.start_polling(
+        bot
+    )
+
+
+
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
