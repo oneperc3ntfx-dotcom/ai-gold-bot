a8026cdc96471e1f552aadb42ce3d5f5f91ce496
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-22 05:18:35 +0700

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,33 +1,29 @@
-from aiogram import Router, F
-from aiogram.types import Message
-
-from handlers.start import start
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-router = Router()
+def main_keyboard():
 
+    keyboard = ReplyKeyboardMarkup(
 
+        keyboard=[
 
-@router.message(
-    F.text=="🚀 START"
-)
-async def start_button(
-    message: Message
-):
+            [
 
-    await start(message)
+                KeyboardButton(
+                    text="🚀 Start"
+                ),
 
+                KeyboardButton(
+                    text="📋 Menu"
+                )
 
+            ]
 
+        ],
 
+        resize_keyboard=True
 
-@router.message(
-    F.text=="⚙️ MENU"
-)
-async def menu_button(
-    message: Message
-):
-
-    await message.answer(
-        "/menu"
     )
+
+
+    return keyboard
