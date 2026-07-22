f6f929fe5dc3503a9cda607abfbafd05f34e7f4b
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-22 09:03:27 +0700

    Update signal.py

--- handlers/signal.py
+++ handlers/signal.py
@@ -1,34 +1,21 @@
 from aiogram import Router, F
-
 from aiogram.types import Message
-
 
 from config.settings import (
     SOURCE_GROUP_ID,
     SIGNAL_TOPIC_ID
 )
 
-
-from services.scheduler import (
-    trading_open
-)
-
-
 from services.membership import (
     get_active_members
 )
-
 
 from services.signal_parser import (
     format_signal
 )
 
 
-
 router = Router()
-
-
-
 
 
 # ==========================
@@ -37,51 +24,37 @@ router = Router()
 
 
 @router.message(
-
     F.chat.id == SOURCE_GROUP_ID
-
 )
-
 async def receive_signal(
-
     message: Message
-
 ):
+
+    print("=== SIGNAL MASUK ===")
+    print("CHAT ID :", message.chat.id)
+    print("TOPIC ID:", message.message_thread_id)
+    print("TEXT    :", message.text)
 
 
     # ======================
     # CHECK TOPIC
     # ======================
 
-
     if message.message_thread_id != SIGNAL_TOPIC_ID:
 
-
+        print("Bukan topic signal")
         return
 
 
 
-
     # ======================
-    # CHECK TRADING TIME
+    # CHECK MESSAGE
     # ======================
-
-
-    if not trading_open():
-
-
-        return
-
-
-
-
 
     if not message.text:
 
-
+        print("Pesan tidak memiliki text")
         return
-
-
 
 
 
@@ -89,14 +62,13 @@ async def receive_signal(
     # FORMAT SIGNAL
     # ======================
 
-
     signal_text = format_signal(
-
         message.text
-
     )
 
 
+    print("SIGNAL FORMAT:")
+    print(signal_text)
 
 
 
@@ -104,10 +76,10 @@ async def receive_signal(
     # GET ACTIVE MEMBERS
     # ======================
 
-
     members = get_active_members()
 
 
+    print("TOTAL MEMBER:", len(members))
 
 
 
@@ -115,25 +87,27 @@ async def receive_signal(
     # BROADCAST
     # ======================
 
-
     for member in members:
+
+        telegram_id = member["Telegram ID"]
 
 
         try:
 
-
             await message.bot.send_message(
 
-                chat_id=
-
-                int(member["Telegram ID"]),
-
+                chat_id=int(telegram_id),
 
                 text=signal_text,
 
-
                 parse_mode="HTML"
 
+            )
+
+
+            print(
+                "TERKIRIM KE:",
+                telegram_id
             )
 
 
@@ -141,11 +115,7 @@ async def receive_signal(
 
 
             print(
-
-                "FAILED SEND:",
-
-                member["Telegram ID"],
-
+                "GAGAL KIRIM:",
+                telegram_id,
                 e
-
             )
